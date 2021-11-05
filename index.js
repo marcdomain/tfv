46d37cc3575ea361f616e442de4ef74dce44f6a6
marcus chukwuoma <marcus2cu@gmail.com>
2021-11-05 11:13:17 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+const yargs = require('yargs')
+const {join} = require('path')
+
+yargs
+	.commandDir(join(__dirname, 'lib', 'commands'))
+	.alias('help', 'h')
+	.scriptName('tfversion')
+	.demandCommand(1, 'You need at least one command before moving on')
+	.strictOptions()
+	// .strictCommands()
+	.showHelpOnFail()
+	.argv
